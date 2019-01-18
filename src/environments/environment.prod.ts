9a5bfda81b1689fa8b51e24d21e97fe135f2e289
VarunReddy <varunreddy2424@gmail.com>
2019-01-19 01:53:54 +0530

    Added lazy loading to training module part-2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyAA5pGJIJAjxj1BbBPhPUlsPF_LEUUrAPo",
-    authDomain: "fitness-tracker-a0b71.firebaseapp.com",
-    databaseURL: "https://fitness-tracker-a0b71.firebaseio.com",
-    projectId: "fitness-tracker-a0b71",
-    storageBucket: "fitness-tracker-a0b71.appspot.com",
-    messagingSenderId: "896423179686"
-  }
 };
